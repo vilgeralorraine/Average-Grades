b7710012aa4b725e1554db09aadab8397160ce54
vilgeralorraine <165258193+vilgeralorraine@users.noreply.github.com>
2024-06-10 20:20:49 +0800

    Add files via upload

---
+++ Average Grades.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace AVERAGE_VILGERA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double g1;
+            double g2;
+            double result;
+
+            Console.Write("Enter first grade: ");
+            g1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second grade: ");
+            g2 = Convert.ToDouble(Console.ReadLine());
+
+            result = GetMean(g1, g2);
+            Console.WriteLine(result);
+
+            double average = result;
+            double remarks;
+            remarks = GetRem(average);
+            Console.WriteLine(remarks);
+        }
+
+        public static double GetMean(double g1, double g2)
+        {
+            double mean = (g1 + g2) / 2;
+            return mean;
+        }
+        public static double GetRem(double average)
+        {
+            double rem = average;
+
+                if (average <= 50)
+                {
+                    Console.WriteLine("FAILED");
+                }
+                else if (average > 50 && average <= 70)
+                {
+                    Console.WriteLine("FAIR");
+                }
+                else if (average > 70 && average <= 80)
+                {
+                    Console.WriteLine("GOOD");
+                }
+                else if (average > 80 && average <= 90)
+                {
+                    Console.WriteLine("VERY GOOD");
+                }
+                else if (average > 90 && average <= 100)
+                {
+                    Console.WriteLine("EXCELLENT");
+                }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+            return rem;
+        }
+    }
+}
